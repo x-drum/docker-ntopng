868f30359cd26ad28140b8f31b7325d2627a7087
Alessio Cassibba (x-drum) <swapon@gmail.com>
2015-02-12 01:46:19 +0100

    One process one container (move nprobe elsewhere)

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM lucaderi/ntopng-docker:latest
 
 MAINTAINER Alessio Cassibba (x-drum) <swapon@gmail.com>
 
-RUN apt-get update && apt-get install -y nprobe
-
 ADD run.sh /
 
 CMD ["/bin/bash","/run.sh"]
--- run.sh
+++ run.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 set -e
 
-WEB=3000
-COLLECTOR=6343
-
-if [ "$WEB_PORT" ]; then
-  WEB=$WEB_PORT
-fi
-if [ "$COLLECTOR_PORT" ]; then
-  COLLECTOR=$COLLECTOR_PORT
-fi
+: ${WEB_PORT:="3000"}
 
 /etc/init.d/redis-server start
-ntopng -i tcp://127.0.0.1:5556 -d /var/tmp -w $WEB -v >> /dev/null &
-nprobe --collector-port $COLLECTOR --zmq tcp://127.0.0.1:5556
+ntopng -d /var/tmp -w $WEB_PORT -v >> /dev/null &
 
 exec "$@"
