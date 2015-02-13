f2dcee3e24ecc3a6b5bb400ae8f7fc4bcca495cd
Alessio Cassibba <swapon@gmail.com>
2015-02-13 13:04:17 +0100

    update wrapper with new defaults

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-: ${WEB_PORT:="3000"}
-
 /etc/init.d/redis-server start
-ntopng -d /var/tmp -w $WEB_PORT -v >> /dev/null &
 
-exec "$@"
+exec /usr/local/bin/ntopng -d /var/lib/ntop "$@"
